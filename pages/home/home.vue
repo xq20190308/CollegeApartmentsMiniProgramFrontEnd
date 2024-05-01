2523e4402a92f8be247ac1a4ab3ce53c79561469
jiangwan12 <145889804+jiangwan12@users.noreply.github.com>
2024-05-01 08:56:17 +0800

    做了底部tabBar，轮播图在小程序模拟器上不显示，稍后修改

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,9 +1,31 @@
 <template>
-	this is home
+	import { swiper, swiperItem } from 'uni-simple-router';
+
+  <view class="banner">
+    <!-- 是否轮播:indicator-dots  自动播放:autoplay  间隔周期:interval  动画执行周期:duration -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="8000" :duration="1000">
+      <swiper-item v-for="(item, index) in bannerList" :key="index">
+        <img :src="item.url" alt="">
+      </swiper-item>
+    </swiper>
+  </view>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      bannerList: [
+        { url: "static/swiper/swiper_school.jpg" },
+        { url: "static/swiper/swiper_school.jpg" },
+        { url: "static/swiper/swiper_school.jpg" },
+        { url: "static/swiper/swiper_school.jpg" }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
+/* 在这里可以添加一些自定义样式 */
 </style>
--- unpackage/dist/dev/mp-weixin/pages/home/home.js
+++ unpackage/dist/dev/mp-weixin/pages/home/home.js
@@ -1,8 +1,26 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
+const _sfc_main = {
+  data() {
+    return {
+      bannerList: [
+        { url: "static/swiper/swiper_school.jpg" },
+        { url: "static/swiper/swiper_school.jpg" },
+        { url: "static/swiper/swiper_school.jpg" },
+        { url: "static/swiper/swiper_school.jpg" }
+      ]
+    };
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.f($data.bannerList, (item, index, i0) => {
+      return {
+        a: item.url,
+        b: index
+      };
+    })
+  };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/CollegeApartmentsMiniProgramFrontEnd/pages/home/home.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/lenovo/Desktop/智慧社区/CollegeApartmentsMiniProgramFrontEnd/pages/home/home.vue"]]);
 wx.createPage(MiniProgramPage);
