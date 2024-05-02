33312fa205ab23189b316ee399c0de37cf327d7d
Burn-orange <155433321+Burn-orange@users.noreply.github.com>
2024-05-02 15:47:27 +0800

    简单写了一下

--- pages/function/function.vue
+++ pages/function/function.vue
@@ -1,9 +1,43 @@
 <template>
-	this is function
+	<!-- 导航栏区域一 -->
+	<view class="nav-list">
+		<view class="nav-item" v-for="{item,i} in navList" : key = "i">
+			<image src="item.imgPath" class = "nav-img"></image>
+		</view>
+		
+	</view>
 </template>
 
 <script>
+	export default{
+		data() {
+			return{
+				//分类功能区的数据列表
+				navList: [],
+				
+				}
+		}
+	},
+	onload() {
+		//调用方法，获取数组中的数据
+		this.getNavList()
+	},
+	methods: {
+		getNavList() {
+			
+		}
+	}
 </script>
 
 <style>
+	.nav-list{
+		display:flex;
+		justify-content: space-around;
+		margin: 15px 0;
+		
+		.nav-img{
+			width:128rpx;
+			height:140rpx;
+		}
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/function/function.js
+++ unpackage/dist/dev/mp-weixin/pages/function/function.js
@@ -1,8 +1,31 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
+const _sfc_main = {
+  data() {
+    return {
+      //分类功能区的数据列表
+    };
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.f(_ctx.func_1List, (item, i, i0) => {
+      return {
+        a: item.imgPath,
+        b: common_vendor.t(item.name),
+        c: i,
+        d: common_vendor.o(($event) => _ctx.func1Click(item), i)
+      };
+    }),
+    b: common_vendor.f(_ctx.payImgList, (item, i, i0) => {
+      return {
+        a: item.image_src,
+        b: i
+      };
+    }),
+    c: common_vendor.o((...args) => _ctx.hourseRepair && _ctx.hourseRepair(...args)),
+    d: common_vendor.o((...args) => _ctx.commRepair && _ctx.commRepair(...args))
+  };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/CollegeApartmentsMiniProgramFrontEnd/pages/function/function.vue"]]);
 wx.createPage(MiniProgramPage);
