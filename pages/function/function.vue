58cdb775c1f612263c354399e81f0bb9c5fd19a0
jiangwan12 <145889804+jiangwan12@users.noreply.github.com>
2024-05-13 15:29:43 +0800

    .

--- pages/function/function.vue
+++ pages/function/function.vue
@@ -1,72 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <!-- 导航栏区域一 -->
-    <view class="nav-list">
-        <view class="nav-item" v-for="(item,i) in navList" :key="i">
-            <image :src="item.imgPath" class = "nav-img"></image>
-        </view>
-        
-    </view>
-</template>
-
-<script>
-    export default{
-        data() {
-            return{
-                //分类功能区的数据列表
-                navList: [
-									{
-									    imgPath:"../../static/function/class.png"
-									},
-									{
-									    imgPath:"../../static/function/classschedule.png"
-									},
-									{
-									    imgPath:"../../static/function/questionnaire.png"
-									},
-									{
-									    imgPath:"../../static/function/tutointeraction.png"
-									}
-								]
-                }
-        },
-    onload() {
-        //调用方法，获取数组中的数据
-        this.navList = [
-            {
-                imgPath:"../../static/function/class.png"
-            },
-            {
-                imgPath:"../../static/function/classschedule.png"
-            },
-            {
-                imgPath:"../../static/function/questionnaire.png"
-            },
-            {
-                imgPath:"../../static/function/tutointeraction.png"
-            }
-            
-        ]
-    },
-    methods: {
-        
-        }
-    }
-</script>
-
-<style>
-    .nav-list{
-        display:flex;
-        justify-content: space-around;
-        margin: 15px 0;
-        }
-        .nav-img{
-            width:128rpx;
-            height:140rpx;
-        }
-    
-=======
-	
 	<view class="container">
 		<view >
 		<text class="nav-t">学习</text>
@@ -192,5 +124,5 @@ export default {
 	      box-shadow: 0 4rpx 10rpx rgba(0, 0, 0, 0.1); /* 添加阴影 */
 		  font-size: 20px;
 	    }
->>>>>>> f9621ead623975911c1cfbbb373534460caa9a1a
+
 </style>
--- unpackage/dist/dev/mp-weixin/pages/function/function.js
+++ unpackage/dist/dev/mp-weixin/pages/function/function.js
@@ -3,47 +3,66 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      //分类功能区的数据列表
-      navList: [
-        {
-          imgPath: "../../static/function/class.png"
-        },
-        {
-          imgPath: "../../static/function/classschedule.png"
-        },
-        {
-          imgPath: "../../static/function/questionnaire.png"
-        },
-        {
-          imgPath: "../../static/function/tutointeraction.png"
-        }
+      navList1: [
+        { name: "课程表", imgPath: "../../static/function/class.png" },
+        { name: "教室预约", imgPath: "../../static/function/classes.png" },
+        { name: "导师互动", imgPath: "../../static/function/mentor.png" },
+        { name: "更多", imgPath: "../../static/function/more.png" }
+      ],
+      navList2: [
+        { name: "问卷投票", imgPath: "../../static/function/questionnaire.png" },
+        { name: "卫检成绩", imgPath: "../../static/function/score.png" },
+        { name: "接诉即办", imgPath: "../../static/function/complaint.png" },
+        { name: "更多", imgPath: "../../static/function/more.png" }
+      ],
+      navList3: [
+        { name: "热水系统", imgPath: "../../static/function/water.png" },
+        { name: "公寓用电", imgPath: "../../static/function/electricity.png" },
+        { name: "失物招领", imgPath: "../../static/function/find.png" },
+        { name: "更多", imgPath: "../../static/function/more.png" }
       ]
     };
   },
-  onload() {
-    this.navList = [
-      {
-        imgPath: "../../static/function/class.png"
-      },
-      {
-        imgPath: "../../static/function/classschedule.png"
-      },
-      {
-        imgPath: "../../static/function/questionnaire.png"
-      },
-      {
-        imgPath: "../../static/function/tutointeraction.png"
-      }
+  onLoad() {
+    this.navList1 = [
+      { name: "课程表", imgPath: "../../static/function/class.png" },
+      { name: "教室预约", imgPath: "../../static/function/classes.png" },
+      { name: "导师互动", imgPath: "../../static/function/mentor.png" },
+      { name: "更多", imgPath: "../../static/function/more.png" }
+    ], this.navList2 = [
+      { name: "问卷投票", imgPath: "../../static/function/questionnaire.png" },
+      { name: "卫检成绩", imgPath: "../../static/function/score.png" },
+      { name: "接诉即办", imgPath: "../../static/function/complaint.png" },
+      { name: "更多", imgPath: "../../static/function/more.png" }
+    ], this.navList3 = [
+      { name: "热水系统", imgPath: "../../static/function/water.png" },
+      { name: "公寓用电", imgPath: "../../static/function/electricity.png" },
+      { name: "失物招领", imgPath: "../../static/function/find.png" },
+      { name: "更多", imgPath: "../../static/function/more.png" }
     ];
-  },
-  methods: {}
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: common_vendor.f($data.navList, (item, i, i0) => {
+    a: common_vendor.f($data.navList1, (item, i, i0) => {
       return {
         a: item.imgPath,
-        b: i
+        b: common_vendor.t(item.name),
+        c: i
+      };
+    }),
+    b: common_vendor.f($data.navList2, (item, i, i0) => {
+      return {
+        a: item.imgPath,
+        b: common_vendor.t(item.name),
+        c: i
+      };
+    }),
+    c: common_vendor.f($data.navList3, (item, i, i0) => {
+      return {
+        a: item.imgPath,
+        b: common_vendor.t(item.name),
+        c: i
       };
     })
   };
