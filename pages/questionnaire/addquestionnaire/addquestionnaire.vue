5eab5c2211a1c9e4b427551397fbb8d23f8b6b5a
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-05-23 17:41:31 +0800

    完成了创建问卷的初步开发

--- pages/questionnaire/addquestionnaire/addquestionnaire.vue
+++ pages/questionnaire/addquestionnaire/addquestionnaire.vue
@@ -1,6 +1,56 @@
 <template>
+	<view>类型：{{newNaire.type}}</view>
+	<view>名称：{{newNaire.name}}</view>
+	<view>描述：{{newNaire.descr}}</view>
+	<view>开始时间：{{newNaire.startTime}}</view>
+	<view>结束时间：{{newNaire.endTime}}</view>
+	<view class="nairetype">
+		<input @input="(e) => qnameChange(e,qindex)" placeholder="请输入题目类型" placeholder-class="answerplacehoder" />
+	</view>
+	<view class="nairename">
+		<input @input="(e) => qnameChange(e,qindex)" placeholder="请输入题目名称" placeholder-class="answerplacehoder" />
+	</view>
+	<view class="nairedescri">
+		<input @input="(e) => qnameChange(e,qindex)" placeholder="请输入题目描述" placeholder-class="answerplacehoder" />
+	</view>
+	<view class="nairrest">
+		<input @input="(e) => qnameChange(e,qindex)" placeholder="请输入题目开始时间" placeholder-class="answerplacehoder" />
+	</view>
+	<view class="naireet">
+		<input @input="(e) => qnameChange(e,qindex)" placeholder="请输入题目结束时间" placeholder-class="answerplacehoder" />
+	</view>
+	<view class="questionsform">
+		<view class="questionitem" v-for="(que,qindex) in questionList" :key="qindex">
+			<view v-if="que.type===1">{{qindex + 1}}.单选题</view>
+			<view v-else-if="que.type===2">{{qindex + 1}}.多选题</view>
+			<view v-else>{{qindex + 1}}.问答题</view>
+			<view class="answer">
+				<input @input="(e) => qnameChange(e,qindex)" placeholder="请输入题目名称" placeholder-class="answerplacehoder" />
+			</view>
+			<view class="answer">
+				<input @input="(e) => qdescriChange(e,qindex)" placeholder="请输入题目描述" placeholder-class="answerplacehoder" />
+			</view>
+			<view class="choice" v-if="que.type===1">
+				<view style="display: flex; flex-wrap: nowrap; margin-bottom: 2px;" v-for="(item, index) in que.content" :key="index">
+					<view style="background-color: white; width: 20px;height: 20px;border-radius: 50px; border: 1px solid #7f7f7f; margin-right: 5px;"></view>
+					<input  @input="(e) => qchitemChange(e,qindex)" placeholder="请输入选项" placeholder-class="answerplacehoder" />
+				</view>
+			</view>
+			<view class="mulchoice"  v-else-if="que.type===2">
+				<view style="display: flex; flex-wrap: nowrap; margin-bottom: 2px;" v-for="(item, index) in que.content" :key="index">
+					<view style="background-color: white; width: 20px;height: 20px; border: 1px solid #7f7f7f; margin-right: 5px;"></view>
+					<input @input="(e) => qmulchitemChange(e,qindex)" placeholder="请输入选项" placeholder-class="answerplacehoder" />
+				</view>
+			</view>
+		</view>
+	</view>
+	<view class="handlequestion">
+		<button class="add" @click="(e)=>add(e,1)">创建单选</button>
+		<button class="add" @click="(e)=>add(e,2)">创建多选</button>
+		<button class="add" @click="(e)=>add(e,3)">创建问答</button>
+	</view>
 	<view>
-		<button class="submit" type="primary" @click="submit">创建</button>
+		<button class="submit" @click="submit">创建</button>
 	</view>
 </template>
 <script>
@@ -11,7 +61,7 @@
 			return {
 				//传到后端的数据
 				newNaire:{//传到问卷列表页面中的数据
-					descr: null,
+					descr: "null",
 					endTime: "2000-03-17 20:08:53",
 					id: "51",
 					name: "与素系办",
@@ -30,6 +80,18 @@
 			}
 		},
 		methods: {
+			add(e,option){
+				console.log(option);
+				this.questionList.push({
+					content: ["A", "B", "C"],
+					describe: "",
+					id: "",
+					name: "",
+					questionnaire: "",
+					type: option,
+				});
+				console.log(this.questionList)
+			},
 			submit() {
 				//提交到后端
 				uni.request({
@@ -59,8 +121,34 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .submit{
-	top: 80px;
+	margin-top: 20px;
+	background-color:#008cff;
+	width: 80%;
+}
+.handlequestion{
+	margin-top: 20px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
+.add{
+	font-size: small;
+	background-color:#008cff;
+	width: 25%;
+}
+.questionsform{
+	margin-left: 10px;
+	margin-right: 10px;
+	display: flex;
+	flex-wrap: wrap;
+}
+.questionitem{
+	width: 100%;
+	margin-top: 10px;
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
 }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/questionnaire/addquestionnaire/addquestionnaire.js
+++ unpackage/dist/dev/mp-weixin/pages/questionnaire/addquestionnaire/addquestionnaire.js
@@ -6,7 +6,7 @@ const _sfc_main = {
       //传到后端的数据
       newNaire: {
         //传到问卷列表页面中的数据
-        descr: null,
+        descr: "null",
         endTime: "2000-03-17 20:08:53",
         id: "51",
         name: "与素系办",
@@ -25,6 +25,18 @@ const _sfc_main = {
     };
   },
   methods: {
+    add(e, option) {
+      console.log(option);
+      this.questionList.push({
+        content: ["A", "B", "C"],
+        describe: "",
+        id: "",
+        name: "",
+        questionnaire: "",
+        type: option
+      });
+      console.log(this.questionList);
+    },
     submit() {
       common_vendor.index.request({
         url: "",
@@ -50,8 +62,54 @@ const _sfc_main = {
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: common_vendor.o((...args) => $options.submit && $options.submit(...args))
+    a: common_vendor.t($data.newNaire.type),
+    b: common_vendor.t($data.newNaire.name),
+    c: common_vendor.t($data.newNaire.descr),
+    d: common_vendor.t($data.newNaire.startTime),
+    e: common_vendor.t($data.newNaire.endTime),
+    f: common_vendor.o((e) => _ctx.qnameChange(e, _ctx.qindex)),
+    g: common_vendor.o((e) => _ctx.qnameChange(e, _ctx.qindex)),
+    h: common_vendor.o((e) => _ctx.qnameChange(e, _ctx.qindex)),
+    i: common_vendor.o((e) => _ctx.qnameChange(e, _ctx.qindex)),
+    j: common_vendor.o((e) => _ctx.qnameChange(e, _ctx.qindex)),
+    k: common_vendor.f($data.questionList, (que, qindex, i0) => {
+      return common_vendor.e({
+        a: que.type === 1
+      }, que.type === 1 ? {
+        b: common_vendor.t(qindex + 1)
+      } : que.type === 2 ? {
+        d: common_vendor.t(qindex + 1)
+      } : {
+        e: common_vendor.t(qindex + 1)
+      }, {
+        c: que.type === 2,
+        f: common_vendor.o((e) => _ctx.qnameChange(e, qindex), qindex),
+        g: common_vendor.o((e) => _ctx.qdescriChange(e, qindex), qindex),
+        h: que.type === 1
+      }, que.type === 1 ? {
+        i: common_vendor.f(que.content, (item, index, i1) => {
+          return {
+            a: common_vendor.o((e) => _ctx.qchitemChange(e, qindex), index),
+            b: index
+          };
+        })
+      } : que.type === 2 ? {
+        k: common_vendor.f(que.content, (item, index, i1) => {
+          return {
+            a: common_vendor.o((e) => _ctx.qmulchitemChange(e, qindex), index),
+            b: index
+          };
+        })
+      } : {}, {
+        j: que.type === 2,
+        l: qindex
+      });
+    }),
+    l: common_vendor.o((e) => $options.add(e, 1)),
+    m: common_vendor.o((e) => $options.add(e, 2)),
+    n: common_vendor.o((e) => $options.add(e, 3)),
+    o: common_vendor.o((...args) => $options.submit && $options.submit(...args))
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "C:/Users/86187/Desktop/CollegeApartmentsMiniProgramFrontEnd/pages/questionnaire/addquestionnaire/addquestionnaire.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-09980780"], ["__file", "C:/Users/86187/Desktop/CollegeApartmentsMiniProgramFrontEnd/pages/questionnaire/addquestionnaire/addquestionnaire.vue"]]);
 wx.createPage(MiniProgramPage);
