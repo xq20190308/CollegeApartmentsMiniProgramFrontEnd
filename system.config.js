2bd775331c2b77f6879b211171958d72b77e9566
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-05-26 00:16:18 +0800

    OK了

--- system.config.js
+++ system.config.js
@@ -1,8 +1,8 @@
 //用来放接口的
 export default {
 	// 开发 测试 用
-	developUrl: 'http://127.0.0.1:4523/m1/4414254-4059226-default',
-	bkDevelopUrl: 'http://localhost:8080',
+	developUrl: 'http://localhost:8080',
+	bkDevelopUrl: 'http://127.0.0.1:4523/m1/4414254-4059226-default',
 	fileUrl: '',
 	// main 分支提交的测试数据: 
 	baseUrl: ''
