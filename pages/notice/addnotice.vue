8ab98e194ac625ff235e40f3772f7589341e8efa
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-05-28 00:02:52 +0800

    添加了一个日期和时间选择功能

--- pages/notice/addnotice.vue
+++ pages/notice/addnotice.vue
@@ -8,23 +8,14 @@
 	<view style="display: flex;flex-wrap: nowrap;">
 		通知内容（文本）：<input @input="(e) => {data.newNotice.publishTime=newdata(e.detail.value);}" placeholder="请" placeholder-class="answerplacehoder" />
 	</view>
-	<view class="uni-title uni-common-pl">日期选择器</view>
-			<view class="uni-list">
-				<view class="uni-list-cell">
-					<view class="uni-list-cell-left">
-						当前选择
-					</view>
-					<view class="uni-list-cell-db">
-						<picker mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
-							<view class="uni-input">{{date}}</view>
-						</picker>
-					</view>
-				</view>
-			</view>
+	<view class="example-body">
+		<uni-datetime-picker v-model="datetimerange" type="datetimerange" rangeSeparator="至" />
+	</view>
 </template>
 
 <script setup>
-import {reactive,ref} from "vue";
+import {onLoad,onShow} from "@dcloudio/uni-app";
+import {reactive,ref,watch} from "vue";
 import {http} from '@/utils/http'
 const data = reactive({
 	timer:null,//延时器，用于防抖处理
@@ -37,47 +28,9 @@ const data = reactive({
 		title: "修改考试延期3"
 		typeName: "修改紧急"*/
 	},
-            title: 'picker',
-            array: ['中国', '美国', '巴西', '日本'],
-            index: 0,
-            date: currentDate,
-            time: '12:01',
+	
 })
-const bindPickerChange = (e) =>{
-            console.log('picker发送选择改变，携带值为', e.detail.value)
-            data.index = e.detail.value
-        }
-const bindDateChange= (e) => {
-            data.date = e.detail.value
-        }
-const bindTimeChange= (e) => {
-            data.time = e.detail.value
-        }
-const getDate = (type) => {
-            const date = new Date();
-            let year = date.getFullYear();
-            let month = date.getMonth() + 1;
-            let day = date.getDate();
-
-            if (type === 'start') {
-                year = year - 60;
-            } else if (type === 'end') {
-                year = year + 2;
-            }
-            month = month > 9 ? month : '0' + month;
-            day = day > 9 ? day : '0' + day;
-            return `${year}-${month}-${day}`;
-        }
-        const currentDate = getDate({
-            format: true
-        })
-        const startDate = ()=> {
-            return getDate('start');
-        }
-        const endDate = ()=> {
-            return getDate('end');
-        }
-
+const datetimerange=ref([])
 const newdata=(value)=>{
 	clearTimeout(data.timer);
 	data.timer = setTimeout(()=>{
@@ -85,6 +38,12 @@ const newdata=(value)=>{
 	}, 500)
 	return value;
 }
+onLoad(()=>{
+	
+})
+watch(datetimerange,(newValue, oldValue)=>{
+	console.log(datetimerange.value);
+})
 </script>
 
 <style>
