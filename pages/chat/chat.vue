04112a1fad00c03263d3ed3416700154dcb8ca97
William <2092107214@qq.com>
2024-06-06 20:21:37 +0800

    为啥连不上啊。。。。。

--- pages/chat/chat.vue
+++ pages/chat/chat.vue
@@ -1,6 +1,7 @@
 <template>
 	<button @click="get">获取头像</button>
 	<button @click="compress">压缩</button>
+	<button @click="wstest">连接WS</button>
 	<image :src="data.url"></image>
 	<uni-file-picker limit="1" @select="selectUpload" file-mediatype="image" disable-preview>
 		<image :src="data.url" class="avatar" />
@@ -26,37 +27,52 @@
 		file: null,
 	})
 
+	const wstest = async () => {
+		console.log("点击ws")
+		uni.connectSocket({
+			url: "wss://localhost:61098/websocket",
+			method: 'GET'
+		})
+		uni.onSocketOpen((res) => {
+			console.log('WebSocket连接已打开！' + res.data);
+		})
+		console.log("函数完成")
+		uni.onSocketError(function(res) {
+			console.log('WebSocket连接打开失败，请检查！', res);
+		});
 
-	// WebSocket构造函数，创建WebSocket对象
-	let ws = new WebSocket('ws://localhost:8080')
-
-	// 连接成功后的回调函数
-	ws.onopen = function(params) {
-		console.log('客户端连接成功')
-		// 向服务器发送消息
-		ws.send('hello')
-	};
-
-	// 从服务器接受到信息时的回调函数
-	ws.onmessage = function(e) {
-		console.log('收到服务器响应', e.data)
-	};
-
-	// 连接关闭后的回调函数
-	ws.onclose = function(evt) {
-		console.log("关闭客户端连接");
-	};
-
-	// 连接失败后的回调函数
-	ws.onerror = function(evt) {
-		console.log("连接失败了");
-	};
-
-
-	// 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，这样服务端会抛异常。
-	window.onbeforeunload = function() {
-		ws.close();
 	}
+
+	// // WebSocket构造函数，创建WebSocket对象
+	// let ws = new WebSocket('ws://localhost:8080')
+
+	// // 连接成功后的回调函数
+	// ws.onopen = function(params) {
+	// 	console.log('客户端连接成功')
+	// 	// 向服务器发送消息
+	// 	ws.send('hello')
+	// };
+
+	// // 从服务器接受到信息时的回调函数
+	// ws.onmessage = function(e) {
+	// 	console.log('收到服务器响应', e.data)
+	// };
+
+	// // 连接关闭后的回调函数
+	// ws.onclose = function(evt) {
+	// 	console.log("关闭客户端连接");
+	// };
+
+	// // 连接失败后的回调函数
+	// ws.onerror = function(evt) {
+	// 	console.log("连接失败了");
+	// };
+
+
+	// // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，这样服务端会抛异常。
+	// window.onbeforeunload = function() {
+	// 	ws.close();
+	// }
 
 
 
