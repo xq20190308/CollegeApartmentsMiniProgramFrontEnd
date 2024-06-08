1cb6a003ca8ae4027284b30037a12bace6d71c51
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-06-08 22:33:23 +0800

    “好像改完了”

--- utils/socket.js
+++ utils/socket.js
@@ -1,59 +1,64 @@
-export var socketTask='';
-export var socketMsgQueue = [];
-export const wsopen=()=>{
-	if(uni.getStorageSync('token')!=''){
-		socketTask=uni.connectSocket({
-			url: "ws://localhost:8080/websocket",
-			header: {},
-			method: 'GET',
-			success: (e) => {
-				console.log('ws connected ',e);
-			}
-		})//连接
-		socketTask.onOpen((res) => {
-			console.log('Ws open ' + res.data);//后端返回的res
-		 })
-		 socketTask.onMessage(function (res) {
-		   console.log('ws receive ' + res.data);
-		   //let pages = getCurrentPages()
-		   //console.log('--',pages[pages.length -1]);
-		   
-		   if(res.data!="心跳"){
-			   socketMsgQueue.push(res.data);// 设置角标
-				// uni.setTabBarBadge({
-				//   index: 2, // tabIndex，tabbar的哪一项，从0开始
-				//   text: String(socketMsgQueue.length).length>3?'99+':String(socketMsgQueue.length) // 显示的文本，超过99显示成99+
-				// });
-			}
-			 
-		 });
-		 socketTask.onError(function (res) {
-		   console.log('ws error ' + res.errMsg);
-		 });
-		 socketTask.onClose(function (res) {
-		   console.log('ws close ' + res.reson);
-		 })
-	}
-	else{
-		console.log('未登录');
-	}
-}
+export var socketTask = "";
+export var socketMsgQueue = {
+  content: "",
+  length: 0
+};
+export var tabbarPathList = ["/pages/home/home", "/pages/function/function", "/pages/message/message", "/pages/myself/myself"];
+export const wsopen = () => {
+  if (uni.getStorageSync("token") != "") {
+    socketTask = uni.connectSocket({
+      url: "ws://localhost:8080/websocket1",
+      header: {
+        Authorization: uni.getStorageSync("token")
+      },
+      method: "GET",
+      success: (e) => {
+        console.log("ws connected ", e);
+      }
+    });
+    socketTask.onOpen((res) => {
+      console.log("Ws open " + res.data);
+    });
+    socketTask.onMessage(function(res) {
+      console.log("ws receive ", res.data);
+      let pages = getCurrentPages();
+      if (res.data != "biu~biu~" && tabbarPathList.indexOf(pages[pages.length - 1].$page.fullPath) != -1) {
+        socketMsgQueue.content = res.data;
+        socketMsgQueue.length = socketMsgQueue.length + 1;
+        uni.setTabBarBadge({
+          index: 2,
+          // tabIndex，tabbar的哪一项，从0开始
+          text: String(socketMsgQueue.length).length > 3 ? "99+" : String(socketMsgQueue.length)
+          // 显示的文本，超过99显示成99+
+        });
+      }
+    });
+    socketTask.onError(function(res) {
+      console.log("ws error " + res.errMsg);
+    });
+    socketTask.onClose(function(res) {
+      console.log("ws close " + res.reson);
+    });
+  } else {
+    console.log("未登录");
+  }
+};
 export const wssend = (msg) => {
-	socketTask.send({
-	  data:msg,
-	  success:(res)=>{
-		  console.log('ws send successed ',res)
-	  },
-	  fail:(err)=>{
-		  console.log("send fail ",err)
-	  }
-	});
-}//发送
+  socketTask.send({
+    data: msg,
+    success: (res) => {
+      console.log("ws send successed ", res);
+    },
+    fail: (err) => {
+      console.log("send fail ", err);
+    }
+  });
+};
 export const wsclose = () => {
-	socketTask.close({
-		//有待添加
-		success:(res)=>{
-			console.log('ws closed ',res);
-		}
-	});
-}//关闭
+  socketTask.close({
+    //有待添加
+    success: (res) => {
+      console.log("ws closed ", res);
+    }
+  });
+};
