d894b22321eca8b048f6273aaae1fca2b057994f
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-06-19 23:46:25 +0800

    改了一下导师展示

--- pages/chat/chat.vue
+++ pages/chat/chat.vue
@@ -1,14 +1,13 @@
 <template>
 	<view style="display: flex;flex-direction: column;justify-content: space-between;">
-		<view style="text-align: center;margin-bottom: 60px;" id="content"><rich-text :nodes="data.messages"></rich-text></view>
+		<view style="text-align: center;margin-bottom: 82px;" id="content"><rich-text :nodes="data.messages"></rich-text></view>
 		
 		<view class="inputstyle">
 			<uni-easyinput v-model="data.message" type="line" placeholder=""></uni-easyinput>
 			<button style="color:#ffffff;backgroundColor:#008fff;" type="primary" size="mini" @click="mywssent">发射爱心</button>
 		</view>
-		
-		<view id="input" style="height: 40px;"></view>
 	</view>
+	<view id="input" style="height: 0px;"></view>
 </template>
 
 <script setup>
@@ -16,6 +15,8 @@ import { onLoad, onShow } from "@dcloudio/uni-app";
 import { reactive, ref,computed } from "vue";
 import { http, load } from '@/utils/http'
 import { wsclose,wsopen,wssend,socketMsgQueue } from "../../utils/socket.js";
+import { onMounted, onUnmounted } from 'vue';
+
 const position = 'bottom'
 const data = reactive({
 	message:'',
@@ -82,12 +83,12 @@ setInterval(() => {
 	.inputstyle{
 		position: fixed;
 		width: 100%;
-		bottom: 0;
+		bottom: 0px;
 		background-color: #008fff;
 		display: flex;
 		flex-direction: row;
 		padding-top: 2px;
-		padding-bottom: 2px;
+		padding-bottom: 4px;
 		padding-left: 5px;
 	}
 </style>
