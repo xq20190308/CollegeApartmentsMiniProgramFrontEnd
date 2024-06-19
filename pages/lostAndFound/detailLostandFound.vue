70e0934ee6f814e5a60d6428a074055e04410517
jiangwan12 <1433316140@qq.com>
2024-06-19 08:16:31 +0800

    增加了弹窗功能

--- pages/lostAndFound/detailLostandFound.vue
+++ pages/lostAndFound/detailLostandFound.vue
@@ -1,64 +1,89 @@
 <template>
-	<uni-section class="mb-10" :title="data.name" type="line" ></uni-section>
-		<uni-section class="mb-10" title="地点" :sub-title="data.pickLocation" type="circle"></uni-section>
+	<uni-section class="mb-10" :title="data.name" type="line"></uni-section>
+	<uni-section class="mb-10" title="地点" :sub-title="data.pickLocation" type="circle"></uni-section>
 	<uni-section class="mb-10" title="时间" :sub-title="data.pickTime" type="circle"></uni-section>
 	<uni-section class="mb-10" title="描述" :sub-title="data.describes" type="circle"></uni-section>
 	<uni-section class="mb-10" title="联系方式" :sub-title="data.contactobject" type="circle"></uni-section>
-	<view v-for="(item, i) in data.filepath" :key="i" >
+<!-- 	<view v-for="(item, i) in data.filepath" :key="i">
 		<!-- <image src="item"></image> -->
 		<!-- 这里路径没写对，所以他会报错? -->
-		{{i}}是{{item}}
-	</view>
+		<!-- {{i}}是{{item}} -->
+<!-- 	</view> -->
+<br>
+<br>
 	<view class="button-row">
-		<button @click="submit('baseForm')" class="button">通过</button>
-		<button @click="retreat" class="button" style="background-color:green; color: #ffffff;">驳回</button>
+			<button @click="submit('baseForm')" class="button">通过</button>
+			<button class="button" type="primary" @click="inputDialogToggle" 
+				style="background-color:red; color: #ffffff;"><text class="button-text" >驳回</text></button>
+			<view v-if = "data.showReason"  class="dialog-box">
+				<textarea class="dialog-text">   驳回原因：{{ data.reason }}</textarea>
+			</view>
+			
+	</view>
+	<view>
+		<!-- 输入框示例 -->
+		<uni-popup ref="inputDialog" type="dialog">
+			<uni-popup-dialog ref="inputClose" mode="input" title="输入内容" value="对话框预置提示内容" placeholder="请输入内容"
+				@close="close" @confirm="dialogInputConfirm"></uni-popup-dialog>
+		</uni-popup>
 	</view>
 </template>
 
 <script setup>
-	import {goto} from "../../utils/access.js"
-	import {onLoad,onShow} from "@dcloudio/uni-app";
-	import {reactive} from "vue";
-	import {http} from '@/utils/http'
-	
+	import {
+		goto
+	} from "../../utils/access.js"
+	import {
+		onLoad,
+		onShow
+	} from "@dcloudio/uni-app";
+	import {
+		reactive, ref
+	} from "vue";
+	import {
+		http
+	} from '@/utils/http'
 	const data = reactive({
-		describes:'' ,
-		contactobject:'',
-		img:[],
-		pickLocation:'',
-		name:'',
-		pickTime:'',
-		filepath:["@/static/feedback/plus.png"],
+		describes: '',
+		contactobject: '',
+		img: [],
+		pickLocation: '',
+		name: '',
+		pickTime: '',
+		filepath: ["@/static/feedback/plus.png"],
+		reason: '',
+		showReason:false,
 	})
+	//弹窗
+	const inputDialog = ref(null);
 	
-	onLoad((options) =>{
+	const inputDialogToggle = () => {
+		inputDialog.value.open();
+		data.showReason = true;
+	}
+
+	const dialogInputConfirm = (val) => {
+		data.reason = val;
+	}
+	onLoad((options) => {
 		console.log('所有信息', options);
 		//检查filepath是否为数组，结果不是
 		console.log(Array.isArray(options.filepath))
-		data.describes=options.describes;
-		data.contactobject=options.contactobject;
-		data.pickLocation=options.pickLocation;
-		data.pickTime=options.pickTime;
-		data.name=options.name;
+		data.describes = options.describes;
+		data.contactobject = options.contactobject;
+		data.pickLocation = options.pickLocation;
+		data.pickTime = options.pickTime;
+		data.name = options.name;
 		//给他转换成数组
-		data.filepath = options.filepath.split(',');
+		//data.filepath = options.filepath.split(',');
 	})
-	const submitreason =()=>{
-		
-	}
-	const submit=() =>{
-		
-	}
-	
-	const retreat=() =>{
-		onShow(()=>{
-			
-		})
+	const submitreason = () => {
+
 	}
 </script>
 
 <style lang="scss">
-  $uni-success: #18bc37 !default;
+	$uni-success: #18bc37 !default;
 
 	.uni-wrap {
 		flex-direction: column;
@@ -75,29 +100,28 @@
 		margin-bottom: 10px;
 	}
 
-  .decoration{
-    width: 6px;
-    height: 6px;
+	.decoration {
+		width: 6px;
+		height: 6px;
 		margin-right: 4px;
-    border-radius: 50%;
+		border-radius: 50%;
 		background-color: $uni-success;
-  }
-	
+	}
+
 	.button-row {
-		//margin-bottom: 20px;
-		display: flex;
-		justify-content: space-between;
-		/* 用于在按钮之间添加等间距 */
+			display: flex;
+			flex-direction: column; // 修改为垂直排列
+			align-items: stretch; // 使按钮宽度充满容器
+		}
 	
-	}
-	.button {
-		background-color: dodgerblue;
-		color: white;
-		width: 300px;
-		height: 47px;
-		border: 0;
-		font-size: 16px;
-		border-radius: 30px;
-		margin: 10px;
-	}
+		.button {
+			background-color: dodgerblue;
+			color: white;
+			width: 100%; // 使按钮宽度充满容器
+			height: 47px;
+			border: 0;
+			font-size: 16px;
+			border-radius: 30px;
+			margin: 10px 0; // 上下边距，左右边距为0
+		}
 </style>
---
+++ pages/lostAndFound/test_pop.vue
@@ -0,0 +1,225 @@
+	<template>
+		<view class="container">
+
+	
+			<uni-section title="输入框示例" type="line" padding>
+				<view class="dialog-box">
+					<text class="dialog-text">输入内容：{{ data.value }}</text>
+				</view>
+				<button class="button" type="primary" @click="inputDialogToggle"><text
+						class="button-text">输入对话框</text></button>
+	
+			</uni-section>
+			
+	
+
+	
+			<view>
+				<!-- 输入框示例 -->
+				<uni-popup ref="inputDialog" type="dialog">
+					<uni-popup-dialog ref="data.inputClose"  mode="input" title="输入内容"  value="对话框预置提示内容!"
+						placeholder="请输入内容" @confirm="dialogInputConfirm"></uni-popup-dialog>
+					
+				</uni-popup>
+			</view>
+	
+			
+		</view>
+	</template>
+
+
+
+<script setup>
+	// export default {
+	// 	data() {
+	// 		return {
+	// 			type: 'center',
+	// 			msgType: 'success',
+	// 			messageText: '这是一条成功提示',
+	// 			value: ''
+	// 		}
+	// 	},
+	// 	onReady() {},
+	// 	methods: {
+	// 		dialogToggle(type) {
+	// 			this.msgType = type
+	// 			this.$refs.alertDialog.open()
+	// 		},
+	// 		dialogConfirm() {
+	// 			console.log('点击确认')
+	// 			this.messageText = `点击确认了 ${this.msgType} 窗口`
+	// 			this.$refs.message.open()
+	// 		},
+	// 		inputDialogToggle() {
+	// 			this.$refs.inputDialog.open()
+	// 		},
+	// 		dialogClose() {
+	// 			console.log('点击关闭')
+	// 		},
+	// 		dialogInputConfirm(val){
+	// 			this.value = val;
+	// 		}
+	// 		// dialogInputConfirm(val) {
+	// 		// 	uni.showLoading({
+	// 		// 		title: '3秒后会关闭'
+	// 		// 	})
+
+	// 		// 	setTimeout(() => {
+	// 		// 		uni.hideLoading()
+	// 		// 		console.log(val)
+	// 		// 		this.value = val
+	// 		// 		// 关闭窗口后，恢复默认内容
+	// 		// 		this.$refs.inputDialog.close()
+	// 		// 	}, 3000)
+	// 		// },
+	// 		// shareToggle() {
+	// 		// 	this.$refs.share.open()
+	// 		// }
+	// 	}
+	// }
+	import {onLoad, onReady} from "@dcloudio/uni-app";
+	import {reactive, ref} from "vue";
+	import {load,http} from '@/utils/http'
+	
+	const data = reactive({
+		value:'',
+		//inputDialog:null,
+		//inputClose:null,
+	})
+	const inputDialog = ref(null);
+	const inputDialogToggle = () =>{
+		inputDialog.value.open();
+	}
+	
+	const dialogInputConfirm = (val) =>{
+		data.value = val;
+	}
+	//inputClose
+</script>
+
+
+<style lang="scss">
+	@mixin flex {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+	}
+
+	@mixin height {
+		/* #ifndef APP-NVUE */
+		height: 100%;
+		/* #endif */
+		/* #ifdef APP-NVUE */
+		flex: 1;
+		/* #endif */
+	}
+
+	.box {
+		@include flex;
+	}
+
+	.button {
+		@include flex;
+		align-items: center;
+		justify-content: center;
+		flex: 1;
+		height: 35px;
+		margin: 0 5px;
+		border-radius: 5px;
+	}
+
+	.example-body {
+		background-color: #fff;
+		padding: 10px 0;
+	}
+
+	.button-text {
+		color: #fff;
+		font-size: 12px;
+	}
+
+	.popup-content {
+		@include flex;
+		align-items: center;
+		justify-content: center;
+		padding: 15px;
+		height: 50px;
+		background-color: #fff;
+	}
+
+	.popup-height {
+		@include height;
+		width: 200px;
+	}
+
+	.text {
+		font-size: 12px;
+		color: #333;
+	}
+
+	.popup-success {
+		color: #fff;
+		background-color: #e1f3d8;
+	}
+
+	.popup-warn {
+		color: #fff;
+		background-color: #faecd8;
+	}
+
+	.popup-error {
+		color: #fff;
+		background-color: #fde2e2;
+	}
+
+	.popup-info {
+		color: #fff;
+		background-color: #f2f6fc;
+	}
+
+	.success-text {
+		color: #09bb07;
+	}
+
+	.warn-text {
+		color: #e6a23c;
+	}
+
+	.error-text {
+		color: #f56c6c;
+	}
+
+	.info-text {
+		color: #909399;
+	}
+
+	.dialog,
+	.share {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+	}
+
+	.dialog-box {
+		padding: 10px;
+	}
+
+	.dialog .button,
+	.share .button {
+		/* #ifndef APP-NVUE */
+		width: 100%;
+		/* #endif */
+		margin: 0;
+		margin-top: 10px;
+		padding: 3px 0;
+		flex: 1;
+	}
+
+	.dialog-text {
+		font-size: 14px;
+		color: #333;
+	}
+</style>
+
