c337f0bbd2d7832cebc8c8269422f10b7adaad86
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-06-29 21:28:45 +0800

    群发选择对象差不多，缺接口

--- pages/chat/postnotice.vue
+++ pages/chat/postnotice.vue
@@ -1,21 +1,69 @@
 <template>
-	<view class="content">
-	<uni-easyinput type="textarea" :maxlength="-1" v-model="data.content" placeholder="请输入内容" :placeholderStyle="data.placeholderStyle"></uni-easyinput>
-	</view>
 	<view style="margin: 15px;
 				border-radius: 20px;
 				overflow: hidden;
 				box-shadow: 0px 2px 20px rgb(0 0 0 / 5%);" class="list">
 		<uni-list border-full>
-			<uni-list-item style="padding: 2px 8px;" :showArrow="false" title="权限" :righticon="''" >
+			<!-- <uni-list-item v-for="(item,key) in data.receiver" :key="key" style="padding: 2px 8px;" :showArrow="false" :title="key" :righticon="''" >
 				<template v-slot:footer>
-					<uni-data-picker popup-title="" :localdata="storedata.classes"
-					:border="false" :clear-icon="false"
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.classes"
+					:border="false" :placeholder="'请选择'+key" v-model="data.receiver[key]" :map="{text:'text',value:'value'}"
+					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
+					</uni-data-picker>
+				</template>
+			</uni-list-item> -->
+			<uni-list-item style="padding: 2px 8px;" :showArrow="false" title="请选择校区" :righticon="''" >
+				<template v-slot:footer>
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.origin"
+					:border="false" :placeholder="'默认全选'" v-model="data.receiver.campusId" :map="{text:'campusName',value:'campusId'}"
 					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
 					</uni-data-picker>
 				</template>
 			</uni-list-item>
+			<uni-list-item v-if="data.receiver.campusId" style="padding: 2px 8px;" :showArrow="false" title="请选择年级" :righticon="''" >
+				<template v-slot:footer>
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.origin[data.idIndex.campusIndex].grades"
+					:border="false" :placeholder="'默认全选'" v-model="data.receiver.gradeId" :map="{text:'gradeName',value:'gradeId'}"
+					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
+					</uni-data-picker>
+				</template>
+			</uni-list-item>
+			<uni-list-item v-if="data.receiver.gradeId&&data.receiver.campusId" style="padding: 2px 8px;" :showArrow="false" title="请选择学院" :righticon="''" >
+				<template v-slot:footer>
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.origin[data.idIndex.campusIndex].grades[data.idIndex.gradeIndex].colleges"
+					:border="false" :placeholder="'默认全选'" v-model="data.receiver.collegeId" :map="{text:'collegeName',value:'collegeId'}"
+					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
+					</uni-data-picker>
+				</template>
+			</uni-list-item>
+			<uni-list-item v-if="data.receiver.collegeId&&data.receiver.gradeId&&data.receiver.campusId" style="padding: 2px 8px;" :showArrow="false" title="请选择专业" :righticon="''" >
+				<template v-slot:footer>
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.origin[data.idIndex.campusIndex].grades[data.idIndex.gradeIndex].colleges[data.idIndex.collegeIndex].majors"
+					:border="false" :placeholder="'默认全选'" v-model="data.receiver.majorId" :map="{text:'majorName',value:'majorId'}"
+					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
+					</uni-data-picker>
+				</template>
+			</uni-list-item>
+			<uni-list-item v-if="data.receiver.majorId&&data.receiver.collegeId&&data.receiver.gradeId&&data.receiver.campusId" style="padding: 2px 8px;" :showArrow="false" title="请选择班级" :righticon="''" >
+				<template v-slot:footer>
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.origin[data.idIndex.campusIndex].grades[data.idIndex.gradeIndex].colleges[data.idIndex.collegeIndex].majors[data.idIndex.majorIndex].classes"
+					:border="false" :placeholder="'默认全选'" v-model="data.receiver.classId" :map="{text:'className',value:'classId'}"
+					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
+					</uni-data-picker>
+				</template>
+			</uni-list-item>
+			<!-- <uni-list-item style="padding: 2px 8px;" :showArrow="false" title="请选择" :righticon="''" >
+				<template v-slot:footer>
+					<uni-data-picker :clear-icon="true" popup-title="" :localdata="storedata.origin[grades]"
+					:border="false" :placeholder="'请选择学院'" v-model="data.receiver.gradeId" :map="{text:'gradeName',value:'gradeId'}"
+					@change="onchange" @nodeclick="onnodeclick" @popupopened="onpopupopened" @popupclosed="onpopupclosed">
+					</uni-data-picker>
+				</template>
+			</uni-list-item> -->
 		</uni-list>
+	</view>
+	<view class="content">
+	<uni-easyinput type="textarea" :maxlength="-1" v-model="data.content" placeholder="请输入内容" :placeholderStyle="data.placeholderStyle"></uni-easyinput>
 	</view>
 	<button style="left: 150px; top: 15px; color:#ffffff;backgroundColor:#008fff;" type="primary" size="mini" @click="post">发布</button>
 	
@@ -31,10 +79,23 @@ import { getTimeStamp } from "../../utils/time.js";
 import { useUserStore } from "../../store/User.js"
 import { storeToRefs } from 'pinia'
 import { useDataStore } from '../../store/data.js';
+const picker = ref(null)
 const data = reactive({
 	content:"",
 	placeholderStyle:"",
-	receiver:"",
+	idIndex:{
+		campusIndex:0,
+		gradeIndex:0,
+		collegeIndex:0,
+		majorIndex:0,
+	},
+	receiver:{
+		campusId:"",
+		gradeId:"",
+		collegeId:"",
+		majorId:"",
+		classId:"",
+	},
 	dataTree:[],
 })
 const store = useUserStore()
@@ -62,26 +123,51 @@ const post = async()=>{
 	}
 }
 const onnodeclick=(e)=> {
-	//console.log('onnodeclick',e);
-	//console.log("receiver",data.receiver)
+	console.log('onnodeclick',e);
+	console.log("receiver",data.receiver)
 }
 const onpopupopened=(e)=> {
-	//console.log('popupopened',e);
-	//console.log("receiver",data.receiver)
+	console.log('popupopened',e);
+	console.log("receiver",data.receiver)
 }
 const onpopupclosed=(e)=> {
-	//console.log('popupclosed',e);
-	//console.log("receiver",data.receiver)
+	console.log('popupclosed',e);
+	console.log("receiver",data.receiver)
 }
 const onchange=(e)=> {
-	//console.log('onchange:', e);
-	data.receiver=e.detail.value[0].value
+	console.log('onchange:', e);
 	console.log("receiver",data.receiver)
+	data.receiver.gradeId=data.receiver.campusId?data.receiver.gradeId:""
+	data.receiver.collegeId=data.receiver.gradeId&&data.receiver.campusId?data.receiver.collegeId:""
+	data.receiver.majorId=data.receiver.collegeId&&data.receiver.gradeId&&data.receiver.campusId?data.receiver.majorId:""
+	data.receiver.classId=data.receiver.majorId&&data.receiver.collegeId&&data.receiver.gradeId&&data.receiver.campusId?data.receiver.classId:""
+	console.log("receiver",data.receiver)
+	console.log("storedata.origin",storedata.origin)
+	if(data.receiver.campusId){//找到校区对应索引
+		let index = storedata.origin.findIndex(item => item.campusId === data.receiver.campusId);
+		data.idIndex.campusIndex=index
+		console.log("校区的索引",data.idIndex.campusIndex)
+	}
+	if(data.receiver.gradeId){//找到年级对应索引
+		let index = storedata.origin[data.idIndex.campusIndex].grades.findIndex(item => item.gradeId === data.receiver.gradeId);
+		data.idIndex.gradeIndex=index
+		console.log("年级的索引",data.idIndex.gradeIndex)
+	}
+	if(data.receiver.collegeId){//找到学院对应索引
+		let index = storedata.origin[data.idIndex.campusIndex].grades[data.idIndex.gradeIndex].colleges.findIndex(item => item.collegeId === data.receiver.collegeId);
+		data.idIndex.collegeIndex=index
+		console.log("学院的索引",data.idIndex.collegeIndex)
+	}
+	if(data.receiver.majorId){//找到专业对应索引
+		let index = storedata.origin[data.idIndex.campusIndex].grades[data.idIndex.gradeIndex].colleges[data.idIndex.collegeIndex].majors.findIndex(item => item.majorId === data.receiver.majorId);
+		data.idIndex.majorIndex=index
+		console.log("专业的索引",data.idIndex.majorIndex)
+	}
 }
 const storedata = useDataStore()
 onLoad(()=>{
 	//console.log("store.user",store.user)
-	//console.log("storedata.classes",storedata.classes)	
+	console.log("storedata.origin",storedata.origin)	
 })
 </script>
 
--- store/data.js
+++ store/data.js
@@ -3,7 +3,332 @@ import {http} from '../utils/http.js'
 import { reactive,ref,computed } from "vue";
 export const useDataStore = defineStore('Data', ()=>{
 	const classes = ref([])
+	//campus grade college major class 校区 年级 学院 专业 班级
+	const httpdata={
+		campusId:1,
+		gradeId:1,
+		collegeId:1,
+		majorId:1,
+		classId:1,
+	}
+	const origin = ref([{
+		campusId:1,//1校区
+		campusName:"青岛校区",//1校区
+		grades:[{
+			gradeId:1,//1年级
+			gradeName:"2022级",//1年级
+			colleges:[{
+				collegeId:1,//1学院
+				collegeName:"计算机科学与工程学院",//1学院
+				majors:[{//三个专业
+					majorId:1,
+					majorName:"计算机科学与技术",
+					classes:[{
+						classId:1,
+						className:"1班"
+					},{
+						classId:2,
+						className:"2班"
+					},{
+						classId:3,
+						className:"3班"
+					}],
+				},{
+					majorId:2,
+					majorName:"软件工程",
+					classes:[{
+						classId:4,
+						className:"1班"
+					},{
+						classId:5,
+						className:"2班"
+					},{
+						classId:6,
+						className:"3班"
+					}],
+				},{
+					majorId:3,
+					majorName:"物联网",
+					classes:[{
+						classId:7,
+						className:"1班"
+					},{
+						classId:8,
+						className:"2班"
+					},{
+						classId:9,
+						className:"3班"
+					}],
+				}]
+			},{
+				collegeId:2,//2学院
+				collegeNmae:"能源与矿业工程学院",//2学院
+				majors:[{//三个专业
+					majorId:4,
+					majorId:"采矿工程",
+					classes:[{
+						classId:10,
+						className:"1班"
+					},{
+						classId:11,
+						className:"2班"
+					}],
+				},{
+					majorId:5,
+					majorName:"工业工程",
+					classes:[{
+						classId:12,
+						className:"1班"
+					}],
+				},{
+					majorId:6,
+					majorName:"工程力学",
+					classes:[{
+						classId:13,
+						className:"1班"
+					},{
+						classId:14,
+						className:"2班"
+					},{
+						classId:15,
+						className:"3班"
+					}],
+				}]
+			}]},{
+			gradeId:2,//2年级
+			gradeName:"2023级",//2年级
+			colleges:[{
+				collegeId:3,//3学院
+				collegeName:"计算机科学与工程学院",//3学院
+				majors:[{//两个专业
+					majorId:7,
+					majorName:"计算机科学与技术",
+					classes:[{
+						classId:16,
+						className:"1班"
+					},{
+						classId:17,
+						className:"2班"
+					},{
+						classId:18,
+						className:"3班"
+					}]
+				},{
+					majorId:8,
+					majorName:"软件工程",
+					classes:[{
+						classId:19,
+						className:"1班"
+					},{
+						classId:20,
+						className:"2班"
+					},{
+						classId:21,
+						className:"3班"
+					}]
+				}]
+			},{
+				collegeId:4,//2学院
+				collegeNmae:"能源与矿业工程学院",//2学院
+				majors:[{//三个专业
+					majorId:9,
+					majorId:"采矿工程",
+					classes:[{
+						classId:22,
+						className:"1班"
+					},{
+						classId:23,
+						className:"2班"
+					}],
+				},{
+					majorId:10,
+					majorName:"工业工程",
+					classes:[{
+						classId:24,
+						className:"1班"
+					}],
+				},{
+					majorId:11,
+					majorName:"工程力学",
+					classes:[{
+						classId:25,
+						className:"1班"
+					},{
+						classId:26,
+						className:"2班"
+					},{
+						classId:27,
+						className:"3班"
+					}],
+				}]
+			}]}]},{
+		campusId:2,//2校区
+		campusName:"泰安校区",//2校区
+		grades:[{
+			gradeId:3,//3年级
+			gradeName:"2022级",//3年级
+			colleges:[{
+				collegeId:5,//1学院
+				collegeName:"计算机科学与工程学院",//1学院
+				majors:[{//三个专业
+					majorId:12,
+					majorName:"计算机科学与技术",
+					classes:[{
+						classId:28,
+						className:"1班"
+					},{
+						classId:29,
+						className:"2班"
+					},{
+						classId:30,
+						className:"3班"
+					}],
+				},{
+					majorId:13,
+					majorName:"软件工程",
+					classes:[{
+						classId:31,
+						className:"1班"
+					},{
+						classId:32,
+						className:"2班"
+					},{
+						classId:33,
+						className:"3班"
+					}],
+				},{
+					majorId:14,
+					majorName:"物联网",
+					classes:[{
+						classId:34,
+						className:"1班"
+					},{
+						classId:35,
+						className:"2班"
+					},{
+						classId:36,
+						className:"3班"
+					}],
+				}]
+			},{
+				collegeId:6,//2学院
+				collegeName:"经济管理学院",//2学院
+				majors:[{//三个专业
+					majorId:15,
+					majorId:"会计学专业",
+					classes:[{
+						classId:37,
+						className:"1班"
+					},{
+						classId:38,
+						className:"2班"
+					}],
+				},{
+					majorId:16,
+					majorName:"工商管理专业",
+					classes:[{
+						classId:39,
+						className:"1班"
+					}],
+				},{
+					majorId:17,
+					majorName:"投资学",
+					classes:[{
+						classId:40,
+						className:"1班"
+					},{
+						classId:41,
+						className:"2班"
+					},{
+						classId:42,
+						className:"3班"
+					}],
+				}]
+			}]},{
+			gradeId:4,//4年级
+			gradeName:"2023级",//4年级
+			colleges:[{
+				collegeId:7,//1学院
+				collegeName:"计算机科学与工程学院",//1学院
+				majors:[{//三个专业
+					majorId:18,
+					majorName:"计算机科学与技术",
+					classes:[{
+						classId:43,
+						className:"1班"
+					},{
+						classId:44,
+						className:"2班"
+					},{
+						classId:45,
+						className:"3班"
+					}],
+				},{
+					majorId:19,
+					majorName:"软件工程",
+					classes:[{
+						classId:46,
+						className:"1班"
+					},{
+						classId:47,
+						className:"2班"
+					},{
+						classId:48,
+						className:"3班"
+					}],
+				},{
+					majorId:20,
+					majorName:"物联网",
+					classes:[{
+						classId:49,
+						className:"1班"
+					},{
+						classId:50,
+						className:"2班"
+					},{
+						classId:51,
+						className:"3班"
+					}],
+				}]
+			},{
+				collegeId:8,//2学院
+				collegeName:"经济管理学院",//2学院
+				majors:[{//三个专业
+					majorId:21,
+					majorId:"会计学专业",
+					classes:[{
+						classId:52,
+						className:"1班"
+					},{
+						classId:53,
+						className:"2班"
+					}],
+				},{
+					majorId:22,
+					majorName:"工商管理专业",
+					classes:[{
+						classId:54,
+						className:"1班"
+					}],
+				},{
+					majorId:23,
+					majorName:"投资学",
+					classes:[{
+						classId:55,
+						className:"1班"
+					},{
+						classId:56,
+						className:"2班"
+					},{
+						classId:57,
+						className:"3班"
+					}]
+			}]}]}]
+	}])
 	const getclasses = async()=>{//http请求在初始化登录时调用
+		console.log(origin.value)
+		console.log(httpdata)
+		
 		const res = await http('/class/get-all','GET',{},)
 		console.log("getclasses res",res.data)
 		classes.value=[];
@@ -13,85 +338,90 @@ export const useDataStore = defineStore('Data', ()=>{
 				value: String(res.data[i].classId),
 			})
 		}
-	// 	classes.value=[{
-	// 	text: "专业一",
-	// 	value: "1-0-0",
-	// 	children: [{
-	// 		text: "一年级",
-	// 		value: "1-1-0",
-	// 		children: [{
-	// 			text: "1.1.1班",
-	// 			value: "1-1-1"
-	// 		},
-	// 		{
-	// 			text: "1.1.2班",
-	// 			value: "1-1-2"
-	// 		}]
-	// 	},{
-	// 		text: "二年级",
-	// 		value: "1-2-0",
-	// 		children: [{
-	// 			text: "1.2.1班",
-	// 			value: "1-2-1"
-	// 		},
-	// 		{
-	// 			text: "1.2.1班",
-	// 			value: "1-2-2"
-	// 		}]
-	// 	}]
-	// },{
-	// 	text: "专业二",
-	// 	value: "2-0-0",
-	// 	children: [{
-	// 		text: "一年级",
-	// 		value: "2-1-0",
-	// 		children: [{
-	// 			text: "2.1.1班",
-	// 			value: "2-1-1"
-	// 		},
-	// 		{
-	// 			text: "2.1.2班",
-	// 			value: "2-1-2"
-	// 		}]
-	// 	},{
-	// 		text: "二年级",
-	// 		value: "2-2-0",
-	// 		children: [{
-	// 			text: "2.2.1班",
-	// 			value: "2-2-1"
-	// 		},
-	// 		{
-	// 			text: "2.2.2班",
-	// 			value: "2-2-2"
-	// 		}]
-	// 	}]
-	// },{
-	// 	text: "专业三",
-	// 	value: "3-0-0",
-	// 	children: [{
-	// 		text: "一年级",
-	// 		value: "3-1-0",
-	// 		children: [{
-	// 			text: "3.1.1班",
-	// 			value: "3-1-1"
-	// 		},
-	// 		{
-	// 			text: "3.1.2班",
-	// 			value: "3-1-2"
-	// 		}]
-	// 	},{
-	// 		text: "二年级",
-	// 		value: "3-2-0",
-	// 		children: [{
-	// 			text: "3.2.1班",
-	// 			value: "3-2-1"
-	// 		},
-	// 		{
-	// 			text: "3.2.2班",
-	// 			value: "3-2-2"
-	// 		}]
-	// 	}]
-	// },]
+		let final = []
+		for (var i = 0; i < origin.length; i++) {//校区
+			//年级
+			
+		}
+		classes.value=[{
+		text: "专业一",
+		value: 1,
+		children: [{
+			text: "一年级",
+			value: 2,
+			children: [{
+				text: "1.1.1班",
+				value: 3
+			},
+			{
+				text: "1.1.2班",
+				value: 4
+			}]
+		},{
+			text: "二年级",
+			value: 5,
+			children: [{
+				text: "1.2.1班",
+				value: 6
+			},
+			{
+				text: "1.2.1班",
+				value: 7
+			}]
+		}]
+	},{
+		text: "专业二",
+		value: 8,
+		children: [{
+			text: "一年级",
+			value: 9,
+			children: [{
+				text: "2.1.1班",
+				value: 10
+			},
+			{
+				text: "2.1.2班",
+				value: 11
+			}]
+		},{
+			text: "二年级",
+			value: 12,
+			children: [{
+				text: "2.2.1班",
+				value: 13
+			},
+			{
+				text: "2.2.2班",
+				value: 14
+			}]
+		}]
+	},{
+		text: "专业三",
+		value: 15,
+		children: [{
+			text: "一年级",
+			value: 16,
+			children: [{
+				text: "3.1.1班",
+				value: 17
+			},
+			{
+				text: "3.1.2班",
+				value: 18
+			}]
+		},{
+			text: "二年级",
+			value: 19,
+			children: [{
+				text: "3.2.1班",
+				value: 20
+			},
+			{
+				text: "3.2.2班",
+				value: 21
+			}]
+		}]
+	},]
 	}
-	return {classes,getclasses}
+	return {classes,getclasses,origin}
 })
